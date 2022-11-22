be55f0a2566888843b57aba63688794134a2e1e4
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2022-11-22 15:41:32 -0800

    Update FirstUnique.java

--- java/FirstUnique.java
+++ java/FirstUnique.java
@@ -1,14 +1,23 @@
+/*
+Given a string s, find the first non-repeating 
+character in it and return its index. 
+If it does not exist, return -1.
+*/
+
 class Solution {
     public int firstUniqChar(String s) {
-        String wrong = "";
+        String left = "";
         
         for (int i = 0; i < s.length(); i++){
+            // take the current char and substring after the current index
             char curChar = s.charAt(i);
             String substring = s.substring(i+1, s.length());
-            if (!substring.contains(curChar + "") && !wrong.contains(curChar + "")) {
+            if (!substring.contains(curChar + "") && !left.contains(curChar + "")) {
+                // if the substring before or after contain this char, return the index of the char
                 return i;
             } else {
-                wrong += curChar;
+                // otherwise, add the char to the left substring
+                left += curChar;
             }
         }
         return -1;
