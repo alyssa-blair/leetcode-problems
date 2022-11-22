d00783b6c3c903156db818deb476b55893b83934
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2022-11-22 15:38:26 -0800

    Update FindFirstOccurrence.java

--- java/FindFirstOccurrence.java
+++ java/FindFirstOccurrence.java
@@ -1,8 +1,17 @@
+/*
+Given two strings needle and haystack, 
+return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+*/
+
+
 class Solution {
     public int strStr(String haystack, String needle) {
         if (!haystack.contains(needle)) {
+            // if the string doesn't contain a substring that has needle, return -1
             return -1;
         } else {
+            // otherwise, return the index of the first occurrence
             return haystack.indexOf(needle);
         }
     }
