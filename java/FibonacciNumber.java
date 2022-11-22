8a303a2afbb376250b48b7b352df0d4b12661536
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2022-11-22 14:48:41 -0800

    Update and rename FibonacciNumber to FibonacciNumber.java

--- java/FibonacciNumber.java
+++ java/FibonacciNumber.java
@@ -1,17 +1,29 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, 
+called the Fibonacci sequence, such that each number is the sum of 
+the two preceding ones, starting from 0 and 1. 
+
+Given n, calculate F(n)
+*/
+
 class Solution {
     public int fib(int n) {
+        // check base cases
         if (n == 0) {
             return 0;
         } else if (n == 1) {
             return 1;
         }
+        // otherwise, start recursion
         return fibrec(n,0,1,1);
     }
     
     public int fibrec(int n, int prev1, int prev2, int count) {
         if (count == n) {
+            // if the number of recursions have been reached, return the last value
             return prev2;
         } else {
+            // otherwise, find the new value and call the function again
             int total = prev1+prev2;
             prev1 = prev2;
             prev2 = total;
