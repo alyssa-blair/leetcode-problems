a6ca0c3d5e0b6f574d82c2c3bb85dd0988e7b32c
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2022-11-22 16:02:46 -0800

    Update PowerOfFour.java

--- java/PowerOfFour.java
+++ java/PowerOfFour.java
@@ -1,10 +1,18 @@
+/*
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+*/
+
 class Solution {
     public boolean isPowerOfFour(int n) {
         float val = n;
+        
         while (val > 4) {
             val = val / 4;
         }
-        
+       
         if (val == 1 || val == 4) {
             return true;
         } else {
