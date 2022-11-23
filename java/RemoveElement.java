3c2d08e2faddcef332e6b147335c2be2bb02070c
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2022-11-23 14:18:37 -0800

    Update RemoveElement.java

--- java/RemoveElement.java
+++ java/RemoveElement.java
@@ -1,9 +1,31 @@
+/*
+Given an integer array nums and an integer val, 
+remove all occurrences of val in nums in-place. 
+The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums. 
+More formally, if there are k elements after removing the duplicates, 
+then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying 
+the input array in-place with O(1) extra memory.
+
+*/
+
+
 class Solution {
     public int removeElement(int[] nums, int val) {
+        
         int length = nums.length;
         int count = 0;
         for (int i = 0; i < length; i++) {
             int cur = nums[i];
+            
+            // if the current value is the one we are looking for, remove it
             if (cur == val) {
                 count++;
                 nums = remove(i, nums, length);
@@ -15,6 +37,7 @@ class Solution {
     }
     
     public int[] remove(int i, int[] nums, int length) {
+        // adjust the array to remove the unwanted values
         for (int j = i; j < length - 1; j++) {
             nums[j] = nums[j+1];
         }
