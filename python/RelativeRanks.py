126bce991129c3cd16bb7374dc2a31bf552b54bd
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2023-01-12 14:21:05 -0800

    Update RelativeRanks.py

--- python/RelativeRanks.py
+++ python/RelativeRanks.py
@@ -4,9 +4,14 @@ class Solution:
         medalArray = [0] * len(score)
 
         for i in range(len(score)):
+            # get the highest score in the array
             cur = max(score)
             index = score.index(cur)
-            score[index] = -1        
+            
+            # mark this as visited
+            score[index] = -1     
+            
+            # use a switch to determine ranking
             match placement:
                 case 1:
                     medalArray[index] = "Gold Medal"
@@ -19,5 +24,4 @@ class Solution:
 
             placement += 1
 
-        return medalArray
         return medalArray
