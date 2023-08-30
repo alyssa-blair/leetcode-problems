675c96c3dc475595e9b221553eab744d23ffda69
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2023-08-30 14:35:55 -0700

    Update FibonacciNumber.java

--- java/FibonacciNumber.java
+++ java/FibonacciNumber.java
@@ -9,10 +9,8 @@ Given n, calculate F(n)
 class Solution {
     public int fib(int n) {
         // check base cases
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+        if (n == 0 || n == 1) {
+            return n;
         }
         // otherwise, start recursion
         return fibrec(n,0,1,1);
