3cc73a2f4c293640f2f44b16bbb215fdc66fc642
Alyssa Blair <105877373+alyssa-blair@users.noreply.github.com>
2023-08-30 14:33:02 -0700

    Update CountOddInRange.java

--- java/CountOddInRange.java
+++ java/CountOddInRange.java
@@ -7,11 +7,7 @@ class Solution {
     public int countOdds(int low, int high) {
         int diff = high-low;
         if (diff % 2 == 0) {
-            if (high % 2 == 0) {
-                return diff / 2;
-            } else {
-                return diff / 2 + 1;
-            }
+            return diff / 2 + (high % 2 == 0 ? 0 : 1);
         } else {
             return (diff + 1) / 2;
         }
